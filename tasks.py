6c9da28872fa91ae39b64742e1cda124ed4d3eea
Nick Malaguti <nmalaguti@users.noreply.github.com>
2022-12-08 10:53:21 -0500

    Support day zero for new-day

--- tasks.py
+++ tasks.py
@@ -8,7 +8,10 @@ from invoke import task
 def new_day(c):
     curr_dir = Path(__file__).parent
     last_day = next(reversed(sorted(list(curr_dir.glob("day-*")))), None)
-    next_day = int(last_day.name[4:]) + 1
+    if last_day is None:
+        next_day = 1
+    else:
+        next_day = int(last_day.name[4:]) + 1
     next_day_dir = curr_dir / f"day-{next_day:02d}"
     next_day_dir.mkdir()
     copyfile(curr_dir / "template.py", next_day_dir / f"day_{next_day:02d}.py")
